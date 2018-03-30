4ff01e1ea95cf4b314dca3ddc434bab650303bda
TheKolme <joana.nummelin.a100557@mcast.edu.mt>
2018-03-30 17:52:34 +0200

    Adding Chain
    Add chain next to the player and setting its size value. Also assigning layers

---
+++ Bubble Struggle Replica/Assets/Chain.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chain : MonoBehaviour {
+
+	public Transform player;
+	public static bool isFired;
+
+	// Use this for initialization
+	void Start () {
+		isFired = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(isFired)
+		{
+
+		}else
+		{
+			transform.position = player.position;
+			transform.localScale = new Vector3(1f, 0f, 1f);
+		}
+
+	}
+}
