e3c340448204b3a20f218be1585326bf2b5de4ad
TheKolme <joana.nummelin.a100557@mcast.edu.mt>
2018-04-11 19:35:42 +0200

    win added

--- Bubble Struggle Replica/Assets/Scripts/ChainCollision.cs
+++ Bubble Struggle Replica/Assets/Scripts/ChainCollision.cs
@@ -1,12 +1,24 @@
 ﻿using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class ChainCollision : MonoBehaviour {
+	public int counter = 31;
+
+	
+	void Update()
+	{
+		if (counter <= 0)
+		{
+			SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex);
+		}
+	}
 	void OnTriggerEnter2D(Collider2D col)
 	{
 		Chain.IsFired = false;
 
 		if(col.tag == "Ball")
 		{
+			counter --;
+			Debug.Log(counter);
 			col.GetComponent<Ball>().Split();
 		}
 	}
